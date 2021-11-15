4df105daf742471d6e637351b113f172f57f5bb8
xbepxx@gmail.com <xbepxx@gmail.com>
2021-11-15 19:26:26 +0300

    1

--- HW22/Program.cs
+++ HW22/Program.cs
@@ -13,21 +13,12 @@ namespace HW22
         Реализовать решение  задачи с  использованием механизма  задач продолжения.*/
         //static int[] mas;
         //static int a;
-        static int[] Mass ()
+        static int[] Mass()
         {
             //int a;
             Random random = new Random();
             Console.WriteLine("Введите размер массива:");
             int a = int.Parse(Console.ReadLine());
-            //bool result = int.TryParse(Console.ReadLine(), out a);
-            //if (result)
-            //{
-            //    Console.WriteLine($"Размер массива {a}");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Ошибка ввода. Введите число");
-            //}
             int[] mas = new int[a];
             for (int i = 0; i < a; i++)
             {
@@ -35,42 +26,34 @@ namespace HW22
             }
             return mas;
         }
-        static void Sum(Task task)
+        static void Sum(Task task, object _array)
         {
-            //int[] s = (int[])m;
-            int[] p = Mass();
+            int[] p = (int[])_array;
             int sum = 0;
             foreach (int n in p)
             {
-                sum=sum+n;
+                sum = sum + n;
             }
             Console.WriteLine($"Сумма чисел в массиве равна {sum}");
         }
-        static void Max(Task task3)
+        static void Max(Task task, object _array)
         {
-            int[] p = Mass();
+            int[] p = (int[])_array;
             int max = p.Max();
             Console.WriteLine($"Максимальное значение в массиве: {max}");
         }
         static void Main(string[] args)
         {
             Func<int[]> func = new Func<int[]>(Mass);
-            Task < int[] > task1= new Task<int[]>(func);
-
-
-            Action<Task> action1 = new Action<Task>(Sum);
-            Task task2 = task1.ContinueWith(action1);
-
-            //Action<Task> action1 = new Action<Task, object>(Sum);
-            //Task task2 = task1.ContinueWith(action1, object);
-
-            Action<Task> action2 = new Action<Task>(Max);
-            Task task3 = task2.ContinueWith(action2);
-
+            Task<int[]> task1 = new Task<int[]>(func);
             task1.Start();
-            task1.Wait();
-            task2.Wait();
-            task3.Wait();
+            int[] array = task1.Result;
+
+            Action<Task, object> action1 = new Action<Task, object>(Sum);
+            Task task2 = task1.ContinueWith(action1, array);
+
+            Action<Task, object> action2 = new Action<Task, object>(Max);
+            Task task3 = task2.ContinueWith(action2, array);
             Console.ReadKey();
         }
     }
